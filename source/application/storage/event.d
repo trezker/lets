75519e8194e25f7d4d6fd680d8a90edcd072e135
Anders Andersson <trezker@gmail.com>
2017-10-02 04:55:05 +0200

    Fixed area restricted find

--- source/application/storage/event.d
+++ source/application/storage/event.d
@@ -49,10 +49,14 @@ class Event_storage {
 
 	Event[] Find(EventSearch eventSearch) {
 		auto conditions = Bson([
-			"location.latitude": Bson(["$gte": Bson(eventSearch.location.latitude - eventSearch.radius)]),
-			"location.latitude": Bson(["$lte": Bson(eventSearch.location.latitude + eventSearch.radius)]),
-			"location.longitude": Bson(["$gte": Bson(eventSearch.location.longitude - eventSearch.radius)]),
-			"location.longitude": Bson(["$lte": Bson(eventSearch.location.longitude + eventSearch.radius)])
+			"location.latitude": Bson([
+				"$gte": Bson(eventSearch.location.latitude - eventSearch.radius),
+				"$lte": Bson(eventSearch.location.latitude + eventSearch.radius)
+			]),
+			"location.longitude": Bson([
+				"$gte": Bson(eventSearch.location.longitude - eventSearch.radius),
+				"$lte": Bson(eventSearch.location.longitude + eventSearch.radius)
+			])
 		]);
 		return MongoArray!(Event)(collection, conditions);
 	}
@@ -83,55 +87,50 @@ unittest {
 	}
 }
 
+Event CoordinateEvent(Location l, string t) {
+	Event event;
+	event.userId = "t";
+	event.title = t;
+	event.description = "Description";
+	event.createdTime = Clock.currTime();
+	event.startTime = Clock.currTime();
+	event.endTime = Clock.currTime();
+	event.location = l;
+
+	return event;
+}
+
 //Find events in area should return all events in the area
 unittest {
 	Database database = GetDatabase("test");
 	try {
-		Event eventInside = {
-			userId: "t",
-			title: "Inside",
-			description: "Description",
-			createdTime: Clock.currTime(),
-			startTime: Clock.currTime(),
-			endTime: Clock.currTime(),
-			location: {
-				latitude: 2,
-				longitude: 2
-			}
-		};
-		Event eventOutside = {
-			userId: "t",
-			title: "Outside",
-			description: "Description",
-			createdTime: Clock.currTime(),
-			startTime: Clock.currTime(),
-			endTime: Clock.currTime(),
-			location: {
-				latitude: 6,
-				longitude: 8
-			}
-		};
-
 		auto event_storage = new Event_storage(database);
-		event_storage.Create(eventInside);
-		event_storage.Create(eventOutside);
+		
+		Location location = {latitude: 4,longitude: 4};
+		event_storage.Create(CoordinateEvent(Location(4, 4), "Inside"));
+		event_storage.Create(CoordinateEvent(Location(2, 2), "Inside"));
+		event_storage.Create(CoordinateEvent(Location(5, 5), "Outside"));
+		event_storage.Create(CoordinateEvent(Location(1, 1), "Outside"));
+		event_storage.Create(CoordinateEvent(Location(5, 1), "Outside"));
+		event_storage.Create(CoordinateEvent(Location(1, 5), "Outside"));
 
 		EventSearch search = {
 			location: {
 				latitude: 3,
 				longitude: 3
 			},
-			radius: 2,
+			radius: 1,
 			fromTime: Clock.currTime(),
 			toTime: Clock.currTime()
 		};
 
 		auto events = event_storage.Find(search);
 
+		writeln(events);
+		//assertEqual(2, events.length);
 		foreach(e; events) {
 			assertEqual("Inside", e.title);
 		}
-		writeln(events);
 	}
 	finally {
 		database.ClearCollection("event");
