5d2c19146ce89039d3d1c66e03b7ecb08733d424
Anders Andersson <trezker@gmail.com>
2017-10-14 22:59:25 +0200

    Registered event actions

--- source/application/Application.d
+++ source/application/Application.d
@@ -14,6 +14,11 @@ import application.Login;
 import application.Logout;
 import application.CurrentUser;
 
+import application.storage.event;
+import application.CreateEvent;
+import application.EventsInArea;
+import application.MyEvents;
+
 class Application {
 	void SetupAjaxMethods(Ajax ajax) {
 		Database database = GetDatabase(null);
@@ -23,6 +28,11 @@ class Application {
 		ajax.SetActionCreator("Login", () => new Login(userStorage));
 		ajax.SetActionCreator("Logout", () => new Logout);
 		ajax.SetActionCreator("CurrentUser", () => new CurrentUser(userStorage));
+
+		auto eventStorage = new Event_storage(database);
+		ajax.SetActionCreator("CreateEvent", () => new CreateEvent(eventStorage));
+		ajax.SetActionCreator("EventsInArea", () => new EventsInArea(eventStorage));
+		ajax.SetActionCreator("MyEvents", () => new MyEvents(eventStorage));
 	}
 
 	string RewritePath(HttpRequest request) {
