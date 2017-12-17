22339dad9ce647dbd651281acbca3f5fc5dd4f80
Anders Andersson <trezker@gmail.com>
2017-12-17 15:49:51 +0100

    Testing

--- public/source/js/test/test.js
+++ public/source/js/test/test.js
@@ -1,21 +1,37 @@
+function assertElementExists(selector) {
+	if($("iframe#test").contents().find(selector).length == 0) {
+		console.log(new Error("Element not found. Selector = " + selector));
+	}
+}
+
+function wait(milliseconds) {
+	return new Promise((resolve, reject) => {
+		setTimeout(function() {
+			resolve();
+		}, milliseconds);
+	});
+}
+
 var TestSuite = function() {
 	var self = this;
 	self.iFrameDOM = $("iframe#test").contents();
 
-	self.alert = function() {
-		setTimeout(function(){
-			$(self.iFrameDOM.find("input")[0]).sendkeys("a");
-			$(self.iFrameDOM.find("input")[1]).sendkeys("a");
-			setTimeout(function(){
-				$(self.iFrameDOM.find("button")[1]).click();
-			}, 1000);
-		}, 1000);
+	self.log_in = function() {
+		$(self.iFrameDOM.find("input")[0]).sendkeys("a");
+		$(self.iFrameDOM.find("input")[1]).sendkeys("a");
+
+		wait(1).then(() => {
+			$(self.iFrameDOM.find("button")[1]).click();
+			wait(1000).then(() => {
+				assertElementExists("[data-bind='click: sign_out']");
+			});
+		});
 	}
 };
 
 function iframeLoaded() {
 	var suite = new TestSuite();
-	suite.alert();
+	suite.log_in();
 }
 
 $("#test").attr("src", "/");
